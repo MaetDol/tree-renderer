6973c2982fa0441577a2259a31e900e4b60ecf58
MaetDol <rndka9@gmail.com>
2025-06-17 15:25:39 +0900

    feat, 컨텍스트 선언

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,71 @@
+import { createContext, useState } from "react";
 import { Node, tree } from "./mock/tree";
 
+type FilterContextType = {
+  checked: Set<string>;
+  collapsed: Set<string>;
+  setChecked: (id: string, checked: boolean) => void;
+  setCollapsed: (id: string, collapsed: boolean) => void;
+  isCollapsed: (id: string) => boolean;
+  isChecked: (id: string) => boolean;
+};
+
+const filterContext = createContext<FilterContextType>({
+  checked: new Set<string>(),
+  collapsed: new Set<string>(),
+
+  setChecked: (id: string, checked: boolean) => {},
+  setCollapsed: (id: string, collapsed: boolean) => {},
+
+  isCollapsed: (id: string) => true,
+  isChecked: (id: string) => true,
+});
+
 function App() {
+  const [checked, setChecked] = useState(new Set<string>());
+  const [collapsed, setCollapsed] = useState(new Set<string>());
+
+  const updateChecked = (id: string, value: boolean) => {
+    setChecked((prev) => {
+      const newSet = new Set(prev);
+      if (value) {
+        newSet.add(id);
+      } else {
+        newSet.delete(id);
+      }
+      return newSet;
+    });
+  };
+
+  const updateCollapsed = (id: string, value: boolean) => {
+    setCollapsed((prev) => {
+      const newSet = new Set(prev);
+      if (value) {
+        newSet.add(id);
+      } else {
+        newSet.delete(id);
+      }
+      return newSet;
+    });
+  };
+
+  const isCollapsed = (id: string) => collapsed.has(id);
+  const isChecked = (id: string) => checked.has(id);
+
   return (
     <>
-      <Leaf root={tree} />
+      <filterContext.Provider
+        value={{
+          checked,
+          collapsed,
+          setChecked: updateChecked,
+          setCollapsed: updateCollapsed,
+          isCollapsed,
+          isChecked,
+        }}
+      >
+        <Leaf root={tree} />
+      </filterContext.Provider>
     </>
   );
 }
